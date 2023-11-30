295ba794bb91e8d650ed8288f83be42344edc61f
one-sando <dsando1984@gmail.com>
2023-11-29 22:08:57 -0600

    add server

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Week 6 - RESTful Groceries
 - As discussed in the video the current status of the app should do the CRUD operation and have RESTful services for persisting data.
+
+
+```bash
+# install mongodb
+brew install mongodb-community@7.0
+# start server, dont' start via mongod
+brew services start mongodb-community@7.0
+# database
+mongosh groceries
+# stop server
+brew services stop mongodb-community@7.0
+
+```
---
+++ express_server/server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const dotenv = require('dotenv');
+
+// load environment variables
+dotenv.config();
+
+const dbPort = process.env.DATABASE_PORT || 27017;
+const mongoURL = process.env.DATABASE_URL || "mongodb://localhost";
+const serverPort = process.env.SERVER_PORT || 8080;
+
+// Middleware
+app.use(express.json());
+
+// DB
+mongoose.connect(`${mongoURL}:${dbPort}/groceries`, 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// Model
+const Grocery = mongoose.model('Grocery', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    }
+}));
+  
+// RESTful Api
+
+// Add
+app.post('/api/groceries', async (req, res) => {
+    let grocery = new Grocery({
+        name: req.body.name,
+        quantity: req.body.quantity
+    });
+    grocery = await grocery.save();
+    res.send(grocery);
+});
+  
+// Read
+app.get('/api/groceries', async (req, res) => {
+    const groceries = await Grocery.find();
+    res.send(groceries);
+});
+  
+// Update
+app.put('/api/groceries/:id', async (req, res) => {
+    const grocery = await Grocery.findByIdAndUpdate(req.params.id, 
+    { 
+        name: req.body.name,
+        quantity: req.body.quantity
+    }, { new: true });
+  
+    res.send(grocery);
+});
+  
+// Delete
+app.delete('/api/groceries/:id', async (req, res) => {
+    const grocery = await Grocery.findByIdAndDelete(req.params.id);
+    res.send(grocery);
+});
+  
+
+// Server
+app.listen(serverPort, () => {
+    console.log(`Server is running on port ${serverPort}`);
+});
